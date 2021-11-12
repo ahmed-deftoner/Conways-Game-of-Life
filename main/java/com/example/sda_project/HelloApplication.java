5633ab6a1743716ae3154537859ab6b256fe80ec
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2021-11-12 17:53:35 +0500

    grid changes
    adding another grid,

--- main/java/com/example/sda_project/HelloApplication.java
+++ main/java/com/example/sda_project/HelloApplication.java
@@ -2,7 +2,12 @@ package com.example.sda_project;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +15,33 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Group group=new Group();
+        GridPane grid = new GridPane();
+        GridPane grid2=new GridPane();
+
+        grid.setPadding(new Insets(0));
+        grid.setHgap(0);
+        grid.setVgap(0);
+
+        grid2.setVgap(10);
+        grid2.setPadding(new Insets(10));
+        grid.add(new Button("Start"),12,12);
+        for (int r = 0; r < 10; r++) {
+            for (int c = 0; c < 10; c++) {
+                int number = 10 * r + c;
+                Button button = new Button(String.valueOf(number));
+                grid.add(button, c, r);
+            }
+        }
+
+        ScrollPane scrollPane = new ScrollPane(grid);
+        ScrollPane scrollPane1 = new ScrollPane(grid2);
+
+       // stage.setScene(new Scene(scrollPane));
+       // FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        group.getChildren().add(scrollPane);
+        group.getChildren().add(scrollPane1);
+        Scene scene = new Scene(group, 500, 500);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- main/java/com/example/sda_project/HelloController.java
+++ main/java/com/example/sda_project/HelloController.java
@@ -1,11 +1,15 @@
 package com.example.sda_project;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    private ListCell<Button> list;
+    private static final int h = 10;
 
     @FXML
     protected void onHelloButtonClick() {
